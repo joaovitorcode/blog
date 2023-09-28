fcad9e513ddf222eb1b863f14b233c1f75b21a66
J. Vitor F <joaoaovitor.jobs2001@gmail.com>
2023-09-28 18:12:50 -0300

    feat: some improvements

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/icon.svg" />
+        <meta name="description" content="Vitor's Blog" />
+        <title>Vitor's Blog</title>
+      </Head>
       <body className='bg-neutral-50 dark:bg-neutral-900'>
         <Main />
         <NextScript />
